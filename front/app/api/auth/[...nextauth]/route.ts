746667893025b0b5b9900394ac3c8f752add8ba2
wahei628 <wahei628@gmail.com>
2025-07-07 16:37:49 +0900

    vercelデプロイエラー解消

--- front/app/api/auth/[...nextauth]/route.ts
+++ front/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -19,8 +19,6 @@ export const authOptions = {
       return true
     },
   },
-}
-
-const handler = NextAuth(authOptions)
+})
 
 export { handler as GET, handler as POST }
